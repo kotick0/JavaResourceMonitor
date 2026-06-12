8a60be13af73efcd60ac180fe8a561b172ed5bf6
kotick0 <krystowskipiotr2005@gmail.com>
2026-06-12 20:06:16 +0200

    initialize JavaResourceMonitor project with Maven setup and basic application structure

---
+++ src/test/java/com/kotecku/javaresourcemonitor/JavaResourceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kotecku.javaresourcemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaResourceMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
